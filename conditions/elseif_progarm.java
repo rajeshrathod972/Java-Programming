b33f9731a4fe42597d8e33a45355136d7141e883
Rajesh Rathod <96156957+rajeshrathod972@users.noreply.github.com>
2023-03-05 10:03:33 +0530

    else if condition in java

---
+++ conditions/elseif_progarm.java
@@ -0,0 +1,15 @@
+public class elseif_progarm {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        if(a>b){
+            System.out.println("A is greater than B");
+        }else if(b>a){
+                 System.out.println("B is greater than A");
+        }
+        else if(c>b){
+            System.out.println("C is greater than B Ans A");
+        }
+    }
+}
