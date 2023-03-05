0af4e24e1bcdd0f1de52450e592f604f9111d952
Rajesh Rathod <96156957+rajeshrathod972@users.noreply.github.com>
2023-03-05 11:03:58 +0530

    while loop in java

---
+++ loops/whileloop.java
@@ -0,0 +1,13 @@
+public class whileloop {
+    public static void main(String[] args) {
+        // 2's table
+
+        int p=2;
+        int i=1;
+        System.out.println("2 s tanle is:");
+        while(i<=10){
+            System.out.println(p*i);
+            i=i+1;
+        }
+    }
+}
