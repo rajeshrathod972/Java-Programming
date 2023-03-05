e17edcafaf88935e59f58a9e7910910214902665
Rajesh Rathod <96156957+rajeshrathod972@users.noreply.github.com>
2023-03-05 11:03:47 +0530

    for loop in java

---
+++ loops/forloop.java
@@ -0,0 +1,12 @@
+
+public class forloop {
+
+    public static void main(String[] args) {
+        int p=2;
+        
+        System.out.println("2 s tanle is:");
+        for(int i=1;i<=10;i++){
+            System.out.println(p*i);
+        }
+    }
+}
