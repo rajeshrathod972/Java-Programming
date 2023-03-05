b73a4b64d116430e1f8d0dc146a13082bbf50113
Rajesh Rathod <96156957+rajeshrathod972@users.noreply.github.com>
2023-03-05 09:58:24 +0530

    if else condition statement in java

---
+++ conditions/if_program.java
@@ -0,0 +1,13 @@
+
+public class if_program {
+public static void main(String[] args) {
+    int a=10;
+    int b=10;
+    if(a>b){
+        System.out.println("B is greater than A");
+    }else{
+          System.out.println("A is greater than B");
+    }
+}
+    
+}
