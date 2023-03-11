32223bfbb02da481be98d366cf036b753dc6bddf
Rajesh Rathod <96156957+rajeshrathod972@users.noreply.github.com>
2023-03-11 22:10:17 +0530

    create a function with parameter

---
+++ functions/withparameter.java
@@ -0,0 +1,12 @@
+package functions;
+
+public class withparameter {
+    static void add(int x, int y){
+        System.out.println(x+y);
+  
+    
+        }
+        public static void main(String[] args) {
+         add(10,80);
+        }
+}
