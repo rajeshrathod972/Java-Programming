d5073617b4d8b723f96cb5084f59634e9ca1524a
Rajesh Rathod <96156957+rajeshrathod972@users.noreply.github.com>
2023-03-11 22:09:56 +0530

    creating function with return type

---
+++ functions/withreturntype.java
@@ -0,0 +1,11 @@
+package functions;
+
+public class withreturntype {
+    static int add(int x, int y){
+        return x+y;
+  
+      }
+      public static void main(String[] args) {
+       System.out.println(add(10,80));
+      }
+}
