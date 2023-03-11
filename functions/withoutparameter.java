2a27d299b3802d9fb40a6613cdf6a092236cc3be
Rajesh Rathod <96156957+rajeshrathod972@users.noreply.github.com>
2023-03-11 22:08:37 +0530

    function without parameter

---
+++ functions/withoutparameter.java
@@ -0,0 +1,13 @@
+package functions;
+
+public class withoutparameter {
+    static void add(){
+        int x=10;
+        int y=20;
+        System.out.println(x+y);
+  
+      }
+      public static void main(String[] args) {
+       add();
+      }
+}
